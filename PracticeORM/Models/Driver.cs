2b9a44cbaa559ac7adadf12c47f9d86f6d717aef
DevXCoder47 <nbelokrinitskiy@ukr.net>
2024-10-29 21:42:41 +0200

    Add project files.

---
+++ PracticeORM/Models/Driver.cs
@@ -0,0 +1,12 @@
+﻿namespace PracticeORM.Models
+{
+    public class Driver
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string? DriverLicence { get; set; }
+        public double Salary { get; set; }
+    }
+}
