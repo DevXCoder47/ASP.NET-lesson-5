4ed1a320969d21219915ef0ad5925d3072e90a1f
DevXCoder47 <nbelokrinitskiy@ukr.net>
2024-10-30 19:47:13 +0200

    DBO added

---
+++ PracticeORM/DBOs/DriverDBO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PracticeORM.DBOs
+{
+    public class DriverDBO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string? DriverLicence { get; set; }
+        public double Salary { get; set; }
+    }
+}
